1aa2b30e27e8c3ae0704436a0069cfec0dc7adbb
David Tetreau <david_tetreau@yahoo.com>
2022-04-07 20:16:20 -0500

    changed wsgi

--- newsproject/wsgi.py
+++ newsproject/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/Desktop/code-platoon/week-14/personal_project/newsproject') 
-load_dotenv(os.path.join(project_folder, '.env'))
+
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings.local")
+
 application = get_wsgi_application()
